e6e45e8f3fb4cc0802be27deb158f938f6033276
İsa Zobu <isaazobu@gmail.com>
2024-05-19 01:24:46 +0300

    feat: add redis client

---
+++ src/config/redis.ts
@@ -0,0 +1,17 @@
+import { createClient } from 'redis';
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+const redisClient = createClient({
+  url: process.env.REDIS_URL || 'redis://localhost:6379',
+});
+
+redisClient.on('error', (err) => console.error('Redis Client Error', err));
+
+redisClient.connect().then(() => {
+    console.log('Redis connected');
+});
+
+
+export default redisClient;
