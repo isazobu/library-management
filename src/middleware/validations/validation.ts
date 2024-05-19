826cd3e2ffa2b23eda88ca64edd4c6b088bbae86
İsa Zobu <isaazobu@gmail.com>
2024-05-19 14:33:48 +0300

    feat: add validation function

---
+++ src/middleware/validations/validation.ts
@@ -0,0 +1,13 @@
+import Joi from 'joi';
+import { Request, Response, NextFunction } from 'express';
+
+
+export function validate(schema: Joi.ObjectSchema) {
+    return (req: Request, res: Response, next: NextFunction) => {
+        const { error } = schema.validate(req.body);
+        if (error) {
+            return res.status(400).json({ message: error.details[0].message });
+        }
+        next();
+    };
+}
