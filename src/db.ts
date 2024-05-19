4d4f5393c90dd67d2b0a2a31aa96cb365ced9af9
İsa Zobu <isaazobu@gmail.com>
2024-05-19 17:42:42 +0300

    chore: get db params from env variable

--- src/db.ts
+++ src/db.ts
@@ -1,10 +1,17 @@
 
 const { Sequelize } = require('sequelize');
+import dotenv from 'dotenv';
 
-export const sequelize = new Sequelize('case_study', 'postgres', 'mysecretpassword', {
-    host: 'localhost',
-    dialect: 'postgres'
-  });
+dotenv.config();
+export const sequelize = new Sequelize(
+    process.env.DB_NAME,
+    process.env.DB_USER,
+    process.env.DB_PASSWORD,
+    {
+        host: process.env.DB_HOST,
+        dialect: 'postgres'
+    }
+);
 
 
 sequelize.authenticate().then(() => {
